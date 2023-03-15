b7142a8fa94ef7b1a97cb57cdc1d5ee0a34cae1c
20211TIIMI0206 <cardosoilanna96@gmail.com>
2023-03-15 12:51:13 -0300

    Atividade 1 Comentário das telas

--- app/src/main/java/cardoso/reis/primeiroapp/MainActivity.java
+++ app/src/main/java/cardoso/reis/primeiroapp/MainActivity.java
@@ -8,21 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { // Criação da classe herdando os valores de uma outra classe chamada AppCompatActivity
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {  // Método onCreate, método que toda vez quando a activity é criada ele é chamado
+        super.onCreate(savedInstanceState); //
         setContentView(R.layout.activity_main);
-        Button btnEnviar =  findViewById(R.id.btnEnviar);
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
+        Button btnEnviar =  findViewById(R.id.btnEnviar); //tipo Button com a váriavel do botão que está na interface, recebe uma função que captura a id do botão que está na interface
+        btnEnviar.setOnClickListener(new View.OnClickListener() { //Ouvidor de clicks, assim que o botão for clicado ele será ativado
             @Override
-            public void onClick(View view) {
-                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
-                String texto = etDigiteAqui.getText().toString();
-                Intent intent = new Intent(MainActivity.this,ProximaActivity.class);
-                intent.putExtra("texto", texto);
-                startActivity(intent);
+            public void onClick(View view) { //Método acionado assim que o ouvidor for ativado
+                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui); //variável do tipo EditText e o etDigiteAqui da interface, novamente a id será capturada dessa mesma variável
+                String texto = etDigiteAqui.getText().toString(); //criando uma String chamada texto que recebe o get do etDigiteAqui
+                Intent intent = new Intent(MainActivity.this,ProximaActivity.class); //Cria uma intenção que vai do MainActivity até a ProximaActivity, ou seja, de uma interface a outra
+                intent.putExtra("texto", texto); //leva os elementos do texto que foi inserido na linha 22 em um dicionário
+                startActivity(intent); //Inicia a intenção
             }
         });
     }
--- app/src/main/java/cardoso/reis/primeiroapp/ProximaActivity.java
+++ app/src/main/java/cardoso/reis/primeiroapp/ProximaActivity.java
@@ -12,9 +12,9 @@ public class ProximaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proxima);
-        Intent i = getIntent();
-        String text = i.getStringExtra("texto");
-        TextView tv = findViewById(R.id.tvExibir);
-        tv.setText(text);
+        Intent i = getIntent(); //armazena a intenção na variavel i
+        String text = i.getStringExtra("texto"); //Adiciona o valor da chave
+        TextView tv = findViewById(R.id.tvExibir);  //recebe o id da tvExibir que está na interface
+        tv.setText(text);  //coloca o text no tv
     }
 }
